0ffa4f3b37d1ad2fb33eb80f21c44d3dd8747505
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-06-19 09:22:00 -0400

    escribiendo css en el styled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,58 @@
 
 import { useState } from 'react'
 import styled from '@emotion/styled'
-
+import ImagenCripto from './img/imagen-criptos.png'
 
 const Heading = styled.h1`
-            font-family: 'Lato', sans-serif;
-            color: #FFF
+      font-family: 'Lato', sans-serif;
+      color: #FFF;
+      text-align: center;
+      font-weigth: 700;
+      margin-top: 80px;
+      margin-bottom:50px;
+      font-size: 34px;
+
+      &::after {
+        content:'';
+        width: 100px;
+        height: 6px;
+        background-color: #66A2FE;
+        display:block;
+        margin: 10px auto 0 auto;
+      }
 ` 
+const Imagen = styled.img`
+    max-width: 400px;
+    width: 80%;
+    margin: 100px auto 0 auto;
+    display:block;
+` 
+
+const Contenedor = styled.div`
+    max-width: 900px;
+    margin: 0 auto;
+    width: 90%;
+    @media(min-width: 992px){
+      display:grid;
+      grid-template-columns:repeat(2, 1fr);
+      column-gap: 2rem;
+    }
+` 
+
 
 function App() {
   
   return (
     <>
-      <Heading>Desde APP</Heading>
+      <Contenedor>
+        <Imagen 
+          src={ImagenCripto}
+          alt="Imagenes de criptomoneda"
+        />
+        <div>
+          <Heading>Cotiza Criptomonedas al Instante</Heading>
+        </div>
+      </Contenedor>
     </>
   )
 }
