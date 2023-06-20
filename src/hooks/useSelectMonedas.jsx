b507d214d0bd8ba8da82b5cc51d834b80cc34f64
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-06-19 20:51:22 -0400

    creando un custom hook

---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,11 @@
+
+const useSelectMonedas = () => {
+  
+    const SelectMonedas = () =>{
+        console.log('Dsde select Monedas')
+    }
+
+    return [ SelectMonedas ];
+}
+
+export default useSelectMonedas
